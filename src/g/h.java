906a5e5d77d9a28e26292ad35442b250ac405abd
DiptiVerma <dipti7595@gmail.com>
2020-09-14 23:35:37 +0530

    my first commit arraylist prog

---
+++ src/g/h.java
@@ -0,0 +1,12 @@
+package g;
+
+import java.util.ArrayList;
+
+public class h {
+  public static void main(String[] args) {
+ArrayList<String> list = new ArrayList<String>();
+list.add("RED");
+list.add("Yellow");
+System.out.println(list);
+ }
+}
